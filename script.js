67c122929d3cb224f9c73a82ce159cf5377e4b5a
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-09-21 20:57:36 +0900

    v-modelにオブジェクトを与えた場合、真偽値が適用されるのか確認

---
+++ script.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      actresses: [
+        { name: "内田真礼", age: 32, hometown: "東京都", checked: false },
+        { name: "東山奈央", age: 29, hometown: "東京都", checked: false },
+        { name: "石原夏織", age: 28, hometown: "千葉県", checked: false },
+        { name: "鬼頭明里", age: 26, hometown: "愛知県", checked: false },
+        { name: "和氣あず未", age: 27, hometown: "東京都", checked: false },
+        { name: "三森すずこ", age: 35, hometown: "東京都", checked: true },
+        { name: "南條愛乃", age: 36, hometown: "静岡県", checked: true },
+      ],
+    };
+  },
+});
+
+app.mount("#app");
